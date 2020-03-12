6c2714705ada9e2cb78bc404dddd27c31e53da69
Chris Farmer <farmerc@udel.edu>
2020-03-12 16:08:23 -0400

    sychronize threads

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -1,5 +1,9 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class MonkeyTypewriter {
     public static void main(String[] args) {
         String introduction = "It was the best of times,\n" +
@@ -23,6 +27,17 @@ public class MonkeyTypewriter {
         // Do all of the Monkey / Thread building here
         // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
         // A Tale Of Two Cities.
+        SafeCopier safeCopier = new SafeCopier(introduction);
+        UnsafeCopier unsafeCopier = new UnsafeCopier(introduction);
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Thread safe = new Thread(safeCopier);
+            safe.start();
+            Thread unsafe = new Thread(unsafeCopier);
+            unsafe.start();
+            threads.add(safe);
+            threads.add(unsafe);
+        }
 
 
         // This wait is here because main is still a thread and we want the main method to print the finished copies
@@ -32,7 +47,8 @@ public class MonkeyTypewriter {
         } catch(InterruptedException e) {
             System.out.println("MAIN INTERRUPTED");
         }
-
+        Logger.getGlobal().info("\nSafe Thread\n" + safeCopier.copied);
+        Logger.getGlobal().info("\nUnsafe Thread\n" + unsafeCopier.copied);
         // Print out the copied versions here.
     }
 }
